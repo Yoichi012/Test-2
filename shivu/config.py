ff7f613ed5cfb4462758a6cc2870aa9b40401c30
NALLA933 <piyush960yogi@gmail.com>
2026-01-21 01:01:56 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,8 +8,8 @@ class Config(object):
     TOKEN = "8352347985:AAEHt7hWqwcrg4w56BeltW9WyNr-fuf9n6s"
     mongo_url = "mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
+    SUPPORT_CHAT = "https://t.me/THE_DRAGON_SUPPORT"
+    UPDATE_CHAT = "https://t.me/PICK_X_UPDATE"
     BOT_USERNAME = "Collect_Em_AllBot"
     CHARA_CHANNEL_ID = "-1003150808065"
     api_id = 35660683
