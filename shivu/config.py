9345a82a87e623cedeccf1c0c21ec08563d5aa3b
NALLA933 <piyush960yogi@gmail.com>
2026-01-21 01:11:36 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,9 +8,9 @@ class Config(object):
     TOKEN = "8352347985:AAEHt7hWqwcrg4w56BeltW9WyNr-fuf9n6s"
     mongo_url = "mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "https://t.me/THE_DRAGON_SUPPORT"
-    UPDATE_CHAT = "https://t.me/PICK_X_UPDATE"
-    BOT_USERNAME = "Collect_Em_AllBot"
+    SUPPORT_CHAT = "THE_DRAGON_SUPPORT"
+    UPDATE_CHAT = "PICK_X_UPDATE"
+    BOT_USERNAME = "New_Waifu_Grab_bot"
     CHARA_CHANNEL_ID = "-1003150808065"
     api_id = 35660683
     api_hash = "7afb42cd73fb5f3501062ffa6a1f87f7"
